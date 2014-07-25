f108fb1f9a31481807a49463a188db468991ed00
lasinducharith <lasinducharith@gmail.com>
2014-07-25 11:04:03 +0530

    Adding nodejs, stdlib and apt puppet modules

--- tools/puppet3/modules/nodejs/manifests/init.pp
+++ tools/puppet3/modules/nodejs/manifests/init.pp
@@ -1,69 +1,140 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-# 
-#   http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+# Class: nodejs
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Usage:
+#
+class nodejs(
+  $dev_package = false,
+  $manage_repo = false,
+  $proxy       = '',
+  $version     = 'present'
+) inherits nodejs::params {
+  #input validation
+  validate_bool($dev_package)
+  validate_bool($manage_repo)
 
-class nodejs {
+  case $::operatingsystem {
+    'Debian': {
+      if $manage_repo {
+        #only add apt source if we're managing the repo
+        include 'apt'
+        apt::source { 'sid':
+          location    => 'http://ftp.us.debian.org/debian/',
+          release     => 'sid',
+          repos       => 'main',
+          pin         => 100,
+          include_src => false,
+          before      => Anchor['nodejs::repo'],
+        }
+      }
+    }
 
-  $target = '/mnt'
+    'Ubuntu': {
+      if $manage_repo {
+        # Only add apt source if we're managing the repo
+        include 'apt'
+        # Only use PPA when necessary.
+        apt::ppa { 'ppa:chris-lea/node.js':
+          before => Anchor['nodejs::repo'],
+        }
 
-  if $stratos_app_path {
-    $nodejs_home = $stratos_app_path
-  } 
-  else {
-    $nodejs_home = "${target}/nodejs"
+        if $dev_package {
+            apt::ppa { 'ppa:chris-lea/node.js-devel':
+              before => Anchor['nodejs::repo'],
+            }
+        }
+      }
+    }
+
+    'Fedora', 'RedHat', 'Scientific', 'CentOS', 'OEL', 'OracleLinux', 'Amazon': {
+      if $manage_repo {
+        package { 'nodejs-stable-release':
+          ensure => absent,
+          before => Yumrepo['nodejs-stable'],
+        }
+        yumrepo { 'nodejs-stable':
+          descr    => 'Stable releases of Node.js',
+          baseurl  => $nodejs::params::baseurl,
+          enabled  => 1,
+          gpgcheck => $nodejs::params::gpgcheck,
+          gpgkey   => 'http://patches.fedorapeople.org/oldnode/stable/RPM-GPG-KEY-tchol',
+          before   => Anchor['nodejs::repo'],
+        }
+        file {'nodejs_repofile':
+          ensure  => 'file',
+          before  => Anchor['nodejs::repo'],
+          group   => 'root',
+          mode    => '0444',
+          owner   => 'root',
+          path    => '/etc/yum.repos.d/nodejs-stable.repo',
+          require => Yumrepo['nodejs-stable']
+        }
+      }
+    }
+
+    'Gentoo': {
+      # Gentoo does not need any special repos for nodejs
+    }
+
+    default: {
+      fail("Class nodejs does not support ${::operatingsystem}")
+    }
   }
 
-  package { ['python-software-properties', 'python', 'g++', 'make']:
-    ensure => installed,
+  # anchor resource provides a consistent dependency for prereq.
+  anchor { 'nodejs::repo': }
+
+  package { 'nodejs':
+    name    => $nodejs::params::node_pkg,
+    ensure  => $version,
+    require => Anchor['nodejs::repo']
   }
 
+  case $::operatingsystem {
+    'Ubuntu': {
+      # The PPA we are using on Ubuntu includes NPM in the nodejs package, hence
+      # we must not install it separately
+    }
 
-  exec {
-    'update-apt':
-    path      => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/', '/usr/local/bin/', '/usr/local/sbin/'],
-    command   => 'apt-get update > /dev/null 2>&1';
+    'Gentoo': {
+      # Gentoo installes npm with the nodejs package when configured properly.
+      # We use the gentoo/portage module since it is expected to be
+      # available on all gentoo installs.
+      package_use { $nodejs::params::node_pkg:
+        ensure  => present,
+        use     => 'npm',
+        require => Anchor['nodejs::repo'],
+      }
+    }
 
-    'add-repo':
-    path      => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/', '/usr/local/bin/', '/usr/local/sbin/'],
-    command   => 'add-apt-repository ppa:chris-lea/node.js > /dev/null 2>&1';
-
-    'Create nodejs home':
-      path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      command => "mkdir -p ${nodejs_home}";
-    
-    'Install libraries':
-      path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      cwd     => "${nodejs_home}",
-      command => 'apt-get install -y nodejs',
-      require => [
-        Exec['update-apt'], 
-        Package['python-software-properties', 'python', 'g++', 'make'],
-	Exec['add-repo'],
-	Exec['update-apt'],
-	Exec['Create nodejs home'],
-      ];
-
-    'Start application':
-      path      => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      cwd       => "${nodejs_home}",
-      onlyif    => 'test -f web.js',
-      command   => 'node web.js > /dev/null 2>&1 &',
-      tries     => 100,
-      try_sleep => 2,
-      require   => Exec['Install libraries'];
+    default: {
+      package { 'npm':
+        name    => $nodejs::params::npm_pkg,
+        ensure  => present,
+        require => Anchor['nodejs::repo']
+      }
+    }
   }
+
+  if $proxy {
+    exec { 'npm_proxy':
+      command => "npm config set proxy ${proxy}",
+      path    => $::path,
+      require => Package['npm'],
+    }
+  }
+
+  if $dev_package and $nodejs::params::dev_pkg {
+    package { 'nodejs-dev':
+      name    => $nodejs::params::dev_pkg,
+      ensure  => $version,
+      require => Anchor['nodejs::repo']
+    }
+  }
+
 }
-
