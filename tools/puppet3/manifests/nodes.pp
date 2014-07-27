a4a9a2b3c61a6ed119e59f52b037749d112ba828
lasinducharith <lasinducharith@gmail.com>
2014-07-27 18:04:03 +0530

    Removing already existing apt module

--- tools/puppet3/manifests/nodes.pp
+++ tools/puppet3/manifests/nodes.pp
@@ -89,8 +89,6 @@ node /mysql/ inherits base {
 # nodejs cartridge node
 node /nodejs/ inherits base {
   require java
-  class {'stdlib':}
-  class {'apt':}
   class {'agent':
     type => 'nodejs',
   }
